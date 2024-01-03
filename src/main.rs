a58157e8d6e8b17061d9599b8c64a383f65df8c0
Michael Jung <MichaelWJung@users.noreply.github.com>
2024-01-03 22:29:49 +0100

    Some cleanups

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,19 @@
 #![no_main]
 #![feature(abi_avr_interrupt)]
 
-use panic_halt as _;
+mod timers;
+
+use crate::timers::{millis, millis_init};
 use arduino_hal::port::mode::PwmOutput;
 use arduino_hal::port::Pin;
 use arduino_hal::simple_pwm::{IntoPwmPin, Prescaler, PwmPinOps, Timer2Pwm};
-use core::cell;
+use avr_device::interrupt::Mutex;
+use core::cell::Cell;
 use core::sync::atomic::{AtomicBool, Ordering};
+use panic_halt as _;
 
 static BUTTON_PRESSED: AtomicBool = AtomicBool::new(false);
-static LAST_TIME_PRESSED: avr_device::interrupt::Mutex<cell::Cell<u32>> =
-    avr_device::interrupt::Mutex::new(cell::Cell::new(0));
+static LAST_TIME_PRESSED: Mutex<Cell<u32>> = Mutex::new(Cell::new(0));
 const DEBOUNCE_DELAY: u32 = 200;
 
 #[avr_device::interrupt(atmega328p)]
@@ -87,61 +90,17 @@ where
     }
 }
 
-const PRESCALER: u32 = 1024;
-const TIMER_COUNTS: u32 = 125;
-
-const MILLIS_INCREMENT: u32 = PRESCALER * TIMER_COUNTS / 16000;
-
-static MILLIS_COUNTER: avr_device::interrupt::Mutex<cell::Cell<u32>> =
-    avr_device::interrupt::Mutex::new(cell::Cell::new(0));
-
-fn millis_init(tc0: &arduino_hal::pac::TC0) {
-    // Configure the timer for the above interval (in CTC mode)
-    // and enable its interrupt.
-    tc0.tccr0a.write(|w| w.wgm0().ctc());
-    tc0.ocr0a.write(|w| w.bits(TIMER_COUNTS as u8));
-    tc0.tccr0b.write(|w| match PRESCALER {
-        8 => w.cs0().prescale_8(),
-        64 => w.cs0().prescale_64(),
-        256 => w.cs0().prescale_256(),
-        1024 => w.cs0().prescale_1024(),
-        _ => panic!(),
-    });
-    tc0.timsk0.write(|w| w.ocie0a().set_bit());
-
-    // Reset the global millisecond counter
-    avr_device::interrupt::free(|cs| {
-        MILLIS_COUNTER.borrow(cs).set(0);
-    });
-}
-
-#[avr_device::interrupt(atmega328p)]
-fn TIMER0_COMPA() {
-    avr_device::interrupt::free(|cs| {
-        let counter_cell = MILLIS_COUNTER.borrow(cs);
-        let counter = counter_cell.get();
-        counter_cell.set(counter + MILLIS_INCREMENT);
-    })
-}
-
-fn millis() -> u32 {
-    avr_device::interrupt::free(|cs| MILLIS_COUNTER.borrow(cs).get())
-}
-
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
 
-    // Configure INT0 for falling edge. 0x03 would be rising edge.
+    // Configure INT0 for rising edge. 0x02 would be falling edge.
     dp.EXINT.eicra.modify(|_, w| w.isc0().bits(0x03));
     // Enable the INT0 interrupt source.
     dp.EXINT.eimsk.modify(|_, w| w.int0().set_bit());
 
     pins.d2.into_pull_up_input();
-
-    // let mut status_led = pins.d13.into_output();
-    // status_led.set_low();
 
     millis_init(&dp.TC0);
     let timer = Timer2Pwm::new(dp.TC2, Prescaler::Prescale64);
@@ -152,9 +111,5 @@ fn main() -> ! {
     loop {
         pulse(&mut pwm_led);
         sos(&mut pwm_led);
-
-        // status_led.set_high();
-        // arduino_hal::delay_ms(1000);
-        // status_led.set_low();
     }
 }
